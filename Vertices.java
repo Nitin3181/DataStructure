aecdb355495e9bb848e4450fcfbdee75bf7dd514
Nitin3181 <31938319+Nitin3181@users.noreply.github.com>
2020-09-17 23:10:22 -0400

    Adding graph files in repo

---
+++ Vertices.java
@@ -0,0 +1,11 @@
+package GraphCreation;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Vertices {
+
+    String label;
+}
